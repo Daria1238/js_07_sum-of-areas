444870151e1b4886d90e899841b61ebe15955817
daria <daria.ruchkina4@gmail.com>
2022-11-06 21:34:14 +0200

    homework7

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function sumOfAreas(squareSide, rectSideA, rectSideB, circleRadius, ellipseMajorAxis, ellipseMinorAxis) {
-	// write your code here
+	return squareSide ** 2 + rectSideA * rectSideB + Math.PI * circleRadius ** 2 + ellipseMajorAxis * ellipseMinorAxis * Math.PI;
 }
 
 module.exports = sumOfAreas;
